607afcc85d14398911fb8e3879a103e688d5b656
julianLapenna12 <7jlapenna@gmail.com>
2023-02-25 11:27:01 -0500

    seeding

--- sample.py
+++ sample.py
@@ -16,7 +16,7 @@ num_samples = 10 # number of samples to draw
 max_new_tokens = 500 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
-seed = 1337
+seed = 1337 + 1
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' # 'float32' or 'bfloat16' or 'float16'
 compile = False # use PyTorch 2.0 to compile the model to be faster
